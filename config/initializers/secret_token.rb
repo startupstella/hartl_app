6a283b0f74cfe69199419711057dc742276f1c54
startupstella <startupstella@gmail.com>
2012-04-23 22:34:19 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HartlApp::Application.config.secret_token = 'b17e02adf4c162a9ef8e9417bccc23293755315d915e2ae6895247e36bf192f011a683a5bdd66c3b5161977a6f059aa6f92948f854de0577be36c7b24a99497a'
