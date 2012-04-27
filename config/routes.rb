3e3e315d431cf401f6186a7e1a9324dec6c8f883
startupstella <startupstella@gmail.com>
2012-04-26 21:28:52 -0500

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,23 @@
 HartlApp::Application.routes.draw do
-  get "static_pages/home"
 
-  get "static_pages/help"
+get "users/new"
+ 
+root to: 'static_pages#home'
 
-	get "static_pages/about"
+ match '/help', to: 'static_pages#help'
+
+match '/about', to: 'static_pages#about'
+	
+match '/contact', to: 'static_pages#contact'
+
+match '/signup', to: 'users#new'
 
   resources :microposts
 
   resources :users
+
+ # You can have the root of your site routed with "root"
+  # just remember to delete public/index.html.
+  # root :to => "welcome#index"
 
 end
