05cef595763bd09148f17bd3bdbd09abff6fa463
startupstella <startupstella@gmail.com>
2012-04-28 12:37:21 -0500

    basic user emodel

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120424035606) do
+ActiveRecord::Schema.define(:version => 20120428172522) do
 
   create_table "microposts", :force => true do |t|
     t.string   "content"
@@ -26,5 +26,7 @@ ActiveRecord::Schema.define(:version => 20120424035606) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
 
 end
